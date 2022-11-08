57d6d0fccbe1d3597d5cfeda9fa1cb923c2041d2
bogomilJelev <76732542+bogomilJelev@users.noreply.github.com>
2022-11-08 18:46:32 +0200

    Zoo

---
+++ zoo/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var gorrila = new Gorilla(Console.ReadLine());
+            Console.WriteLine(gorrila.Name);
+;
+        }
+    }
+}
