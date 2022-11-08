ab41150a57991c2f9c9ca16f3bcda2c051952091
bogomilJelev <76732542+bogomilJelev@users.noreply.github.com>
2022-11-08 18:47:45 +0200

    Player

---
+++ player/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var knight = new Knight("Goshko", 7);
+            System.Console.WriteLine(knight.ToString());
+        }
+    }
+}
