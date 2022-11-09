892a589acdd0ae24a99263b2d8b425352390f307
bogomilJelev <76732542+bogomilJelev@users.noreply.github.com>
2022-11-09 12:56:55 +0200

    Add files via upload

---
+++ need for speed/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var car = new Car(200, 100);
+            var motor = new RaceMotorcycle(200, 100);
+            car.Drive(2);
+            motor.Drive(2);
+        }
+    }
+}
---
+++ need for speed/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+        private double DefaultFuelConsumption =1.25;
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public Vehicle(int horsePower,double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * this.FuelConsumption;
+            Console.WriteLine($"Km driven ==>{kilometers}");
+        }
+    }
+}
